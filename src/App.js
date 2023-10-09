534996f70b6a5d9854b8ff53f7b05c36070ef909
ParikshitGehlaut <139605784+ParikshitGehlaut@users.noreply.github.com>
2023-10-09 21:16:25 +0530

    commiting all changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./My Components/Navbar";
+import Todos from "./My Components/Todos";
+import Footer from "./My Components/Footer";
+import React, { useState } from "react";
 
 function App() {
+  const onDelete = (todo) => {
+    // console.log("Hey!", todo);
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
+  };
+  const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "Go to the market",
+      description: "You need to go the market to bring vegetables",
+    },
+    {
+      sno: 2,
+      title: "Go to the gym",
+      description: "You need to go the market to lose weight",
+    },
+    {
+      sno: 3,
+      title: "Go to the class",
+      description: "You need to go the market to get good marks",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="Todos List" searchBar={true} />
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
